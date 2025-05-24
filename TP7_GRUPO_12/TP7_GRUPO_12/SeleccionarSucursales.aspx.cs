9ab7c3a392e43466f78c02f4e9abdb64e00958f1
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-05-23 22:03:33 -0300

    Cosa innesesaria

--- TP7_GRUPO_12/TP7_GRUPO_12/SeleccionarSucursales.aspx.cs
+++ TP7_GRUPO_12/TP7_GRUPO_12/SeleccionarSucursales.aspx.cs
@@ -107,17 +107,15 @@ namespace TP7_GRUPO_12
 
                 }
 
-                bool existe = tablita.AsEnumerable().Any(row => row.Field<int>("ID_SUCURSAL") == idSucursal);
+                /*bool existe = tablita.AsEnumerable().Any(row => row.Field<int>("ID_SUCURSAL") == idSucursal);
                 if (!existe)
-                {
+                {*/
 
                     tablita.Rows.Add(idSucursal, lblNombre.Text, lblDescripcion.Text);
-                }
+                //}
 
                 //guardamos la tabla
                 claseSESSION.GuardarTablaEnSesion(tablita,Session);
-
-
 
                 //Esto es actualmente innecesario
                 claseSESSION.Sucursal_ID = idSucursal;
