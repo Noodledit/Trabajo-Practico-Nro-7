c796fd44139e7fa40e8eb21dbf855a1205e7b7f4
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-05-23 22:22:07 -0300

    Correccion

--- TP7_GRUPO_12/TP7_GRUPO_12/SeleccionarSucursales.aspx.cs
+++ TP7_GRUPO_12/TP7_GRUPO_12/SeleccionarSucursales.aspx.cs
@@ -107,12 +107,9 @@ namespace TP7_GRUPO_12
 
                 }
 
-                /*bool existe = tablita.AsEnumerable().Any(row => row.Field<int>("ID_SUCURSAL") == idSucursal);
-                if (!existe)
-                {*/
 
-                    tablita.Rows.Add(idSucursal, lblNombre.Text, lblDescripcion.Text);
-                //}
+                tablita.Rows.Add(idSucursal, lblNombre.Text, lblDescripcion.Text);
+               
 
                 //guardamos la tabla
                 claseSESSION.GuardarTablaEnSesion(tablita,Session);
